a56adda05a612dca0969b2dec03f6425f3e2036d
shenzhen2017 <2868405029@qq.com>
2022-04-14 22:31:10 +0800

    ADD: vue header

---
+++ src/components/Header.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="header">
+    <div class="title">Running T-Rex</div>
+    <div class="sub-title">
+      <strong>T-Rex</strong>来自Google Chrome离线提示页面。<br />按 "Space"
+      空格键开始游戏。
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class Header extends Vue {}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.header {
+  display: flex;
+  flex-direction: column;
+}
+
+.title {
+  font-size: 30px;
+  text-align: center;
+  font-family: Helvetica;
+}
+
+.sub-title {
+  margin-top: 5px;
+  text-align: center;
+  color: #8a8a8a;
+  margin-bottom: 80px;
+  font-size: 16px;
+  line-height: 20px;
+  font-family: Georgia;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,27 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <Header />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import Header from "@/components/Header.vue"; // @ is an alias to /src
 
 @Component({
   components: {
-    HelloWorld,
+    Header,
   },
 })
 export default class HomeView extends Vue {}
 </script>
+<style scoped>
+.home {
+  box-sizing: border-box;
+  font-size: 1em;
+  line-height: 1.6em;
+  margin: 50px auto 0;
+  max-width: 600px;
+  width: 100%;
+}
+</style>
