3749f4fd2a804ed29582e15e5716b916c17fbd38
shenzhen2017 <2868405029@qq.com>
2022-04-15 09:10:16 +0800

    ADD: road animation

---
+++ src/types/game-types.ts
@@ -0,0 +1,3 @@
+export enum GameConfig {
+  ROAD_VELOCITY = 5,
+}
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,20 +2,47 @@
   <div class="home">
     <Header />
     <div class="game">
-      <img src="@/assets/road.png" />
+      <img src="@/assets/road.png" :style="roadStyle" />
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import { GameConfig } from "@/types/game-types";
 import Header from "@/components/Header.vue"; // @ is an alias to /src
 @Component({
   components: {
     Header,
   },
 })
-export default class HomeView extends Vue {}
+export default class HomeView extends Vue {
+  roadTranslate = 0;
+  timer = 0;
+  get roadStyle() {
+    return { transform: `translateX(${this.roadTranslate}px)` };
+  }
+  created() {
+    this.startGame();
+  }
+
+  startGame() {
+    this.startGamerInterval();
+  }
+
+  startGamerInterval() {
+    this.timer = setInterval(() => {
+      this.updateGameStatus();
+    }, 100);
+  }
+
+  updateGameStatus() {
+    if (this.roadTranslate <= -600) {
+      this.roadTranslate = 0;
+    }
+    this.roadTranslate -= GameConfig.ROAD_VELOCITY;
+  }
+}
 </script>
 <style scoped>
 .home {
@@ -26,5 +53,8 @@ export default class HomeView extends Vue {}
   max-width: 600px;
   width: 100%;
   overflow: hidden;
+}
+.game-road {
+  transform: translate(-700px, 0);
 }
 </style>
